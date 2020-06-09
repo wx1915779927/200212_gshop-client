a8ca7c8090b156db1e8c0c8b730c83a34e4fd324
wuxi <1915779927@qq.com>
2020-06-09 22:56:11 +0800

    '3'

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <TypeNav />
+    <ListContainer />
+    <TodayRecommend />
+    <Rank />
+    <Like />
+    <Floor v-for="floor in floors" :key="floor.id" :floor="floor" />
+    <Brand />
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+import ListContainer from "./ListContainer/ListContainer";
+import TodayRecommend from "./TodayRecommend/TodayRecommend";
+import Rank from "./Rank/Rank";
+import Like from "./Like/Like";
+import Floor from "./Floor/Floor";
+import Brand from "./Brand/Brand";
+
+export default {
+  name: "Home",
+
+  computed: {
+    ...mapState({
+      floors: (state) => state.home.floors,
+    }),
+  },
+
+  mounted() {
+    // 分发给异步action请求获取楼层列表数据
+    this.$store.dispatch("getFloors");
+    this.$store.dispatch("getBanners");
+  },
+
+  // 局部注册组件 (就当前组件使用)
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
